70f940629f9d51aab395fe04440d9e79350d59d3
Juan Buitrago <juanbuitrago2002@outlook.com>
2024-09-26 22:47:16 -0500

    Refactor CSS styles for background and card layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,21 +1,52 @@
-// src/components/Layout.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import logo from "../assets/BetPlay-logo.png";
+import "./Layout.css";
 
 const Layout = () => {
+  const [sessionTime, setSessionTime] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSessionTime((prevTime) => prevTime + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTime = (seconds) => {
+    const hours = String(Math.floor(seconds / 3600)).padStart(2, "0");
+    const minutes = String(Math.floor((seconds % 3600) / 60)).padStart(2, "0");
+    const secs = String(seconds % 60).padStart(2, "0");
+    return `${hours}:${minutes}:${secs}`;
+  };
+
   return (
-    <div>
-      <header>
-        <h1>Mi Aplicación</h1>
-        {/* Aquí puedes agregar más elementos del encabezado */}
+    <div className="layout-container">
+      <header className="layout-header">
+        <div className="header-top">
+          <div className="logo-container">
+            <img src={logo} alt="Betplay logo" className="logo" />
+          </div>
+          <p className="menu-text">Menú</p>
+        </div>
+        <div className="header-bottom">
+          <div className="user-info">
+            <p className="user-name">Juan Buitrago</p>
+            <p className="session-text">Sesión</p>
+            <p className="session-time">{formatTime(sessionTime)}</p>
+          </div>
+          <div className="extra-info">
+            <p className="extra-content">•</p>
+            <p className="extra-content">•</p>
+            <p className="extra-content">•</p>
+          </div>
+        </div>
       </header>
-      <main>
+      <main className="layout-main">
         <Outlet />
       </main>
-      <footer>
-        <p>© 2023 Mi Aplicación</p>
-        {/* Aquí puedes agregar más elementos del pie de página */}
-      </footer>
+      <footer className="layout-footer"></footer>
     </div>
   );
 };
