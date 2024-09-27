17f27515a465d2588128a4eced9a11e20fc35e00
Juan Buitrago <juanbuitrago2002@outlook.com>
2024-09-26 23:37:46 -0500

    Refactor CSS styles for background and card layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import logo from "../assets/BetPlay-logo.png";
+import home from "../assets/home.svg";
+import cards from "../assets/CARDSBET.svg";
+import recargas from "../assets/Frame (1).svg";
+import retiros from "../assets/Group 2904.svg";
 import "./Layout.css";
 
 const Layout = () => {
@@ -25,10 +29,12 @@ const Layout = () => {
     <div className="layout-container">
       <header className="layout-header">
         <div className="header-top">
-          <div className="logo-container">
-            <img src={logo} alt="Betplay logo" className="logo" />
-          </div>
-          <p className="menu-text">Menú</p>
+          <Link to="/" className="menu-link">
+            <div className="logo-container">
+              <img src={logo} alt="Betplay logo" className="logo" />
+            </div>
+            <p className="menu-text">Menú</p>
+          </Link>
         </div>
         <div className="header-bottom">
           <div className="user-info">
@@ -46,7 +52,37 @@ const Layout = () => {
       <main className="layout-main">
         <Outlet />
       </main>
-      <footer className="layout-footer"></footer>
+      <footer className="layout-footer footer-container">
+        <div className="footer-content">
+          <Link to="/" className="footer-link">
+            <div className="footer-logo">
+              <img src={home} alt="Betplay logo" className="logo-footer" />
+            </div>
+            <p className="footer-text">Inicio</p>
+          </Link>
+        </div>
+
+        <div className="footer-content">
+          <Link to="/cards" className="footer-link">
+            <div className="footer-logo">
+              <img src={cards} alt="Betplay logo" className="logo-footer" />
+            </div>
+            <p className="footer-text">Apuestas</p>
+          </Link>
+        </div>
+        <div className="footer-content">
+          <div className="footer-logo">
+            <img src={recargas} alt="Betplay logo" className="logo-footer" />
+          </div>
+          <p className="footer-text">Recargas</p>
+        </div>
+        <div className="footer-content">
+          <div className="footer-logo">
+            <img src={retiros} alt="Betplay logo" className="logo-footer" />
+          </div>
+          <p className="footer-text">Retiros</p>
+        </div>
+      </footer>
     </div>
   );
 };
