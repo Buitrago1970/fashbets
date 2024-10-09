9a4aae6dc6f0d3be8ae67ea6bdbc812b7282b3cd
Juan Buitrago <juanbuitrago2002@outlook.com>
2024-10-08 21:41:08 -0500

    Refactor TimerBar component styles and add blinking animation

--- src/pages/Cards/TimerBar/TimerBar.js
+++ src/pages/Cards/TimerBar/TimerBar.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./TimerBar.css";
 
 function TimerBar({ progressPercentage }) {
+  const isNearFull = progressPercentage >= 90;
+
   return (
     <div className="timer-bar">
       <div
-        className="timer-progress"
+        className={`timer-progress ${isNearFull ? "blink" : ""}`}
         style={{ width: `${progressPercentage}%` }}
       ></div>
     </div>
